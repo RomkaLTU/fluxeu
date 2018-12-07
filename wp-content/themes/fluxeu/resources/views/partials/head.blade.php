73133f131f47b7cd33a3ea2842ff1ca4dcee9f79
Romualdas D <r.daskevicius@gmail.com>
2018-12-07 20:00:55 +0200

    Header develop;

--- wp-content/themes/fluxeu/resources/views/partials/head.blade.php
+++ wp-content/themes/fluxeu/resources/views/partials/head.blade.php
@@ -1,6 +1,7 @@
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  @php wp_head() @endphp
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600&amp;subset=latin-ext" rel="stylesheet">
+    @php wp_head() @endphp
 </head>
--- wp-content/themes/fluxeu/resources/views/partials/header.blade.php
+++ wp-content/themes/fluxeu/resources/views/partials/header.blade.php
@@ -1,10 +1,23 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
-  </div>
+<header class="header">
+    <div class="row no-gutters h-100">
+        <div class="col header-left">
+            <div class="header-left-inner">
+                <a class="brand" href="{{ home_url('/') }}">
+                    <img src="@asset('images/logo.png')" alt="{{ get_bloginfo('name', 'display') }}">
+                </a>
+                <div class="header-content-hero">
+                    <h1>DARBAS OLANDIJOJE</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit. Donec mattis nibh placerat, ornare lacus at, venenatis sapien.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col header-right">
+            <div class="header-right-inner">
+                <nav>
+                    {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+                </nav>
+            </div>
+        </div>
+    </div>
 </header>
