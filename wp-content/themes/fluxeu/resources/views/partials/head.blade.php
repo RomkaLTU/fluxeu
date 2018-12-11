ee78b13ec09449196fa2cc245d4a7e43dfae0652
Romualdas D <romualdas@prodev.lt>
2018-12-11 15:11:37 +0200

    Header;

--- wp-content/themes/fluxeu/resources/views/partials/head.blade.php
+++ wp-content/themes/fluxeu/resources/views/partials/head.blade.php
@@ -2,6 +2,6 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600&amp;subset=latin-ext" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600,700,900&amp;subset=latin-ext" rel="stylesheet">
     @php wp_head() @endphp
 </head>
