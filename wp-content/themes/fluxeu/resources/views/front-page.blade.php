0a4e15eee3f42201a0a5f6706c33154cc162be02
Romualdas D <romualdas@prodev.lt>
2018-12-11 15:11:47 +0200

    Section about;

--- wp-content/themes/fluxeu/resources/views/front-page.blade.php
+++ wp-content/themes/fluxeu/resources/views/front-page.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    test
+    <div class="section section-about" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <h3>APIE MUS</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Suspendisse vitae tempus lectus. Fusce maximus nisl in orci molestie consectetur et vel ante.
+                        Nullam eget ligula sit amet magna viverra aliquet et in sapien. Sed nec leo et tortor facilisis viverra.
+                    </p>
+                </div>
+                <div class="col-lg-6">
+                    <div class="illiustration">
+                        <img src="@asset('images/illustration.png')" alt="Apie mus">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
